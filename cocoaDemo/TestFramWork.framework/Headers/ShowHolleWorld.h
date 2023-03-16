d663bf96bd29b081ee81994ebe276609e04221b3
袁石 <yuans@polysoft.com.cn>
2023-03-16 16:37:23 +0800

    提交 framework

---
+++ cocoaDemo/TestFramWork.framework/Headers/ShowHolleWorld.h
@@ -0,0 +1,18 @@
+//
+//  ShowHolleWorld.h
+//  TestFramWork
+//
+//  Created by Stone袁 on 2023/3/16.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ShowHolleWorld : NSObject
+
+- (void)showHolleWorld;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ cocoaDemo/TestFramWork.framework/Headers/TestFramWork.h
@@ -0,0 +1,19 @@
+//
+//  TestFramWork.h
+//  TestFramWork
+//
+//  Created by Stone袁 on 2023/3/16.
+//
+
+#import <Foundation/Foundation.h>
+#import "ShowHolleWorld.h"
+
+//! Project version number for TestFramWork.
+FOUNDATION_EXPORT double TestFramWorkVersionNumber;
+
+//! Project version string for TestFramWork.
+FOUNDATION_EXPORT const unsigned char TestFramWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFramWork/PublicHeader.h>
+
+
